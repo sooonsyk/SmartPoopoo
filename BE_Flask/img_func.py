ba5b6ab586f316572dc3f177d86d3497f043bd87
sooonsyk <sooyeon0829@ewhain.net>
2024-10-08 11:38:52 +0900

    [1008 sooonsyk] 이미지 전처리 함수 수정

--- BE_Flask/img_func.py
+++ BE_Flask/img_func.py
@@ -1,24 +1,31 @@
-import base64
-from skimage import img_as_ubyte, io
-import numpy as np
+import requests
 from PIL import Image
 import io as io_module
+import numpy as np
+from skimage import img_as_ubyte
+import os
 
 # AWB-white patch algorithm
 def white_patch(image, percentile=100):
     white_patch_image = img_as_ubyte((image * 1.0/np.percentile(image,percentile,axis=(0, 1))).clip(0, 1))
     return white_patch_image
 
-# 이미지 전처리 함수
-def img_preprocess(input_image):
-    
+
+# 이미지 전처리 함수 (이미지 URL 입력 및 처리 후 이미지 경로 반환)
+def img_preprocess(input_image_url, output_path):
+    # URL에서 이미지 다운로드
+    response = requests.get(input_image_url)
+    input_image = Image.open(io_module.BytesIO(response.content))
+
     # 이미지를 불러온 후 AWB 적용
     white_balanced_image = white_patch(input_image)
+
+    # PIL 이미지로 변환
     pil_image = Image.fromarray(white_balanced_image)
 
-    # 메모리 버퍼에 저장
-    buffered = io_module.BytesIO()
-    pil_image.save(buffered, format="PNG")
+    # 처리된 이미지를 파일로 저장
+    output_file = os.path.join(output_path, "processed_image.png")
+    pil_image.save(output_file)
 
-    # base64로 인코딩
-    return base64.b64encode(buffered.getvalue()).decode('utf-8')
+    # 저장된 파일 경로를 반환 (또는 웹 서버를 사용 중이라면 해당 경로의 URL 반환)
+    return output_file
